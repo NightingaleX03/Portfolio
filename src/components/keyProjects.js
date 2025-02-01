7f02d6c0f55cd7d74c5db189cd6020efbc4d0abb
smathew32 <smathew32@myseneca.ca>
2025-02-01 16:23:58 -0500

    fixed a couple issues with the carousal formatting

--- src/components/keyProjects.js
+++ src/components/keyProjects.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import samewise from "../images/samwise.png";
+import backgrounddd from "../images/keyprojectbackground.png";
 import "./keyProjects.css";
 
 const KeyProjects = () => {
@@ -18,45 +18,78 @@ const KeyProjects = () => {
   return (
     <div className="container">
       <div className="slide" ref={slideRef}>
-                <div class="item" style={{ backgroundImage: `url(${samewise})` }}>
+                <div class="item" style={{ backgroundImage: `url(${backgrounddd})` }}>
                     <div class="content">
-                        <div class="name">Switzerland</div>
-                        <div class="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                        <div class="name">Nomore404 App</div>
+                        <div class="des">
+                            <h3><span>Role: Full Stack Developer and AI Integration</span></h3>
+                            <br></br>
+                            <p>
+                                <ul>
+                                    <li>AI powered platform built with Streamlit, PostgreSQL, and Cohere AI provides an algorithm analysis solution for Job Search</li>
+                                    <li>Developed community features, tackled API challenges and worked with python, css, and GenAI</li>
+                                </ul>
+                            </p>
+                        </div>
                         <button>See More</button>
                     </div>
                 </div>
-                <div class="item" style={{ backgroundImage: `url(${samewise})` }}>
+                <div class="item" style={{ backgroundImage: `url(${backgrounddd})` }}>
                     <div class="content">
-                        <div class="name">Finland</div>
-                        <div class="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                        <div class="name">NoteEase App</div>
+                        <div class="des"><h3><span>Role: Full Stack Developer</span></h3>
+                        <br></br>
+                            <p>
+                                <ul>
+                                    <li>AI driven note taking application utilizing Google Cloud Speech-To-Text API and TensorFlow to generate real-time transcription, creating an immersive note taking experience through hand gestures formatting.</li>
+                                    <li>Implemented Google Gemini to improve transcription accuracy and collaborated with team using MERN Stack</li>
+                                </ul>
+                            </p>
+                        </div>
                         <button>See More</button>
                     </div>
                 </div>
-                <div class="item" style={{ backgroundImage: `url(${samewise})` }}>
+                <div class="item" style={{ backgroundImage: `url(${backgrounddd})` }}>
                     <div class="content">
-                        <div class="name">Iceland</div>
-                        <div class="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                        <div class="name">RentEase App</div>
+                        <div class="des"><h3><span>Role: Backend Developer</span></h3>
+                        <br></br>
+                            <p>
+                                <ul>
+                                    <li>Full-stack application using Python and MERN stack to facilitate smooth interactions between homeowners homebuyers</li>
+                                    <li>Integrated AI-powered lawyer on custom database assisting contract generation using HTML to PDF API</li>
+                                </ul>
+                            </p>
+                        </div>
                         <button>See More</button>
                     </div>
                 </div>
-                <div class="item" style={{ backgroundImage: `url(${samewise})` }}>
+                <div class="item" style={{ backgroundImage: `url(${backgrounddd})` }}>
                     <div class="content">
-                        <div class="name">Australia</div>
-                        <div class="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                        <div class="name">3udoku-shuffle</div>
+                        <div class="des"><h3><span>Role: Web Developer</span></h3>
+                        <br></br>
+                            <p>
+                                <ul>
+                                    <li>Developed front-end based website for game dev startup team size of 3 using HTML, CSS, and JavaScript</li>
+                                </ul>
+                            </p>
+                        </div>
                         <button>See More</button>
                     </div>
                 </div>
-                <div class="item" style={{ backgroundImage: `url(${samewise})` }}>
+                <div class="item" style={{ backgroundImage: `url(${backgrounddd})` }}>
                     <div class="content">
-                        <div class="name">Netherland</div>
-                        <div class="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
-                        <button>See More</button>
-                    </div>
-                </div>
-                <div class="item" style={{ backgroundImage: `url(${samewise})` }}>
-                    <div class="content">
-                        <div class="name">Ireland</div>
-                        <div class="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                        <div class="name">Mealer: Food Delivery App</div>
+                        <div class="des"><h3><span>Role: Front-End Developer and Database Developer</span></h3>
+                        <br></br>
+                            <p>
+                                <ul>
+                                    <li>Developed food delivery app Android Studio using Java, for seamless user experience for restaurant owners/customers</li>
+                                    <li>Integrated Google Maps API to allow users to view nearby restaurants and explore available options in their area.</li>
+                                </ul>
+                            </p>
+                        </div>
                         <button>See More</button>
                     </div>
                 </div>
@@ -65,10 +98,10 @@ const KeyProjects = () => {
     
             <div className="button">
                 <button className="prev" onClick={handlePrev}>
-                <i className="fa-solid fa-arrow-left"></i>
+                &#8592;
                 </button>
                 <button className="next" onClick={handleNext}>
-                <i className="fa-solid fa-arrow-right"></i>
+                &#8594;
                 </button>
             </div>
         </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Intro from "../components/Intro";
 import "./css/home.css";
-import samwise from "../images/samwise2.0.png";
 import Footer from "../components/footer";
 import KeyProjects from "../components/keyProjects";
 
@@ -9,26 +8,21 @@ const HomePage = () => {
     return (
         <div>
             <Intro />
-            <br></br>
-            <br></br>
             <div className="button-row">
                 <button className="custom-button">
-                    <img src={samwise} alt="corner" className="corner-image2" />
                     Project
                 </button>
                 <button className="custom-button">
-                    <img src={samwise} alt="corner" className="corner-image2" />
                     Blog Page
                 </button>
                 <button className="custom-button">
-                    <img src={samwise} alt="corner" className="corner-image2" />
                     Contact Me
                 </button>
             </div>
 
-            <section className="key-projects-section"></section>
+            <section className="key-projects-section">
                 <KeyProjects />
-            <section/>
+            </section>
 
             <Footer />
         </div>
